90fdc820114875759c6d7c4e5e850dd057081fcb
zoycun <59949605+zoycun@users.noreply.github.com>
2020-04-30 12:09:01 +0400

    add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const telegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot('1232088235:AAF_LN5eRM_4G56EvB5AJEr8Kfyta_87NOA',{polling: true});
+
+bot.on('message', msg => {
+
+  bot.sendMessage(msg.chat.id, `Привет "${msg.from.first_name}"`)
+  
+});
